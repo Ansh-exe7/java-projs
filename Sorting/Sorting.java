1b4d67cc1f437813f45daa874718ea0c3b9b8cdb
Ansh-exe7 <vermaji014editing@gmail.com>
2025-08-01 12:05:14 +0530

    Added Functionalities

--- Sorting/Sorting.java
+++ Sorting/Sorting.java
@@ -1,8 +1,72 @@
-
 public class Sorting {
 
+    // Bubble Sort
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Selection Sort
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int temp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // Insertion Sort
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        
+        int[] original = {5, 2, 9, 1, 5, 6};
+
+        int[] arr1 = original.clone();
+        System.out.print("Original array: ");
+        printArray(arr1);
+
+        bubbleSort(arr1);
+        System.out.print("Bubble Sorted:  ");
+        printArray(arr1);
+
+        int[] arr2 = original.clone();
+        selectionSort(arr2);
+        System.out.print("Selection Sorted: ");
+        printArray(arr2);
+
+        int[] arr3 = original.clone();
+        insertionSort(arr3);
+        System.out.print("Insertion Sorted: ");
+        printArray(arr3);
     }
 }
-
